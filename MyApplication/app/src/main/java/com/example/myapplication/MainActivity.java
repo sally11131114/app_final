618b4d28643a9bcf265b360a7b6e217d77858faa
sally11131114 <sally11131114@gmail.com>
2023-06-07 05:40:39 +0800

    add_Attendance() add_Friend() done

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -48,7 +48,9 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
 //                login_User_pass = editTextPassword.getText().toString();
 //                Thread thread = new Thread(mutiThread);
 //                thread.start(); // 開始執行
-                add_Contest("upup", 1, "12:30:00", 30, "King", 5, "test.png");
+//                add_Contest("upup", 1, "12:30:00", 30, "King", 5, "test.png");
+//                add_Attendance("sally", "再睡5分鐘", 0);
+//                add_Friend("sally", "marow");
             }
         });
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -250,6 +252,173 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
         AddContestRunnable addContest = new AddContestRunnable();
         addContest.setParams(group_name, type, deadline, expected_amount, award_name, award_score, award_avatar);
         Thread thread = new Thread(addContest);
+        thread.start();
+        return result;
+    };
+
+    public class AddAttendanceRunnable implements Runnable {
+        private String participant_name;
+        private String contest_name;
+        private int score;
+
+        public void setParams(String participant_name, String contest_name, int score) {
+            this.participant_name = participant_name;
+            this.contest_name = contest_name;
+            this.score = score;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://140.116.82.9:9000/addAttendance.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
+                connection.setRequestMethod("POST");
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                Map<String, Object> AttendanceData = new HashMap<>();
+                AttendanceData.put("participant_name", participant_name);
+                AttendanceData.put("contest_name", contest_name);
+                AttendanceData.put("score", score);
+
+                JSONObject response_j = new JSONObject(AttendanceData);
+                Log.v("提交數據", response_j.toString());
+                connection.connect(); // 開始連線
+                OutputStream os = connection.getOutputStream();
+                OutputStreamWriter objout = new OutputStreamWriter(os, "UTF-8");
+                objout.write(response_j.toString());
+                objout.flush();
+                os.close();
+                objout.close();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    InputStream inputStream =
+                            connection.getInputStream();
+                    // 取得輸入串流
+                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
+                    // 讀取輸入串流的資料
+                    String box = ""; // 宣告存放用字串
+                    String line = null; // 宣告讀取用的字串
+                    while((line = bufReader.readLine()) != null) {
+                        box += line + "\n";
+                        // 每當讀取出一列，就加到存放字串後面
+                    }
+                    inputStream.close(); // 關閉輸入串流
+                    result = box; // 把存放用字串放到全域變數
+                    Log.d("HTTP_output", "output!");
+                    Log.d("SUCCESS result", "" +": "+result);
+                } else {
+                    // 处理连接错误
+                    // 例如，您可以获取错误消息
+                    InputStream errorStream = connection.getErrorStream();
+                    String errorMessage = readStream(errorStream); // 将错误流转换为字符串
+                    Log.d("BAD ", errorMessage);
+                    // 处理错误流
+                }
+
+            } catch(Exception e) {
+                result = e.toString(); // 如果出事，回傳錯誤訊息
+                Log.d("BAD result", "" +": "+result);
+            }
+        }
+    }
+    private String add_Attendance(String participant_name, String contest_name, int score){
+        AddAttendanceRunnable addAttendance = new AddAttendanceRunnable();
+        addAttendance.setParams(participant_name, contest_name, score);
+        Thread thread = new Thread(addAttendance);
+        thread.start();
+        return result;
+    };
+
+    public class AddFriendRunnable implements Runnable {
+        private String my_name;
+        private String friend_name;
+
+        public void setParams(String my_name, String friend_name) {
+            this.my_name = my_name;
+            this.friend_name = friend_name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://140.116.82.9:9000/addFriend.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
+                connection.setRequestMethod("POST");
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                Map<String, Object> FriendData = new HashMap<>();
+                FriendData.put("my_name", my_name);
+                FriendData.put("friend_name", friend_name);
+
+                JSONObject response_j = new JSONObject(FriendData);
+                Log.v("提交數據", response_j.toString());
+                connection.connect(); // 開始連線
+                OutputStream os = connection.getOutputStream();
+                OutputStreamWriter objout = new OutputStreamWriter(os, "UTF-8");
+                objout.write(response_j.toString());
+                objout.flush();
+                os.close();
+                objout.close();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    InputStream inputStream =
+                            connection.getInputStream();
+                    // 取得輸入串流
+                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
+                    // 讀取輸入串流的資料
+                    String box = ""; // 宣告存放用字串
+                    String line = null; // 宣告讀取用的字串
+                    while((line = bufReader.readLine()) != null) {
+                        box += line + "\n";
+                        // 每當讀取出一列，就加到存放字串後面
+                    }
+                    inputStream.close(); // 關閉輸入串流
+                    result = box; // 把存放用字串放到全域變數
+                    Log.d("HTTP_output", "output!");
+                    Log.d("SUCCESS result", "" +": "+result);
+                } else {
+                    // 处理连接错误
+                    // 例如，您可以获取错误消息
+                    InputStream errorStream = connection.getErrorStream();
+                    String errorMessage = readStream(errorStream); // 将错误流转换为字符串
+                    Log.d("BAD ", errorMessage);
+                    // 处理错误流
+                }
+
+            } catch(Exception e) {
+                result = e.toString(); // 如果出事，回傳錯誤訊息
+                Log.d("BAD result", "" +": "+result);
+            }
+        }
+    }
+    private String add_Friend(String my_name, String friend_name){
+        AddFriendRunnable addFriend = new AddFriendRunnable();
+        addFriend.setParams(my_name, friend_name);
+        Thread thread = new Thread(addFriend);
         thread.start();
         return result;
     };
