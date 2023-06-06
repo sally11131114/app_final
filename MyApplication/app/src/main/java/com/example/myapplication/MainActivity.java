8431b01e91978f9d906b190666023163422dd24c
sally11131114 <sally11131114@gmail.com>
2023-06-07 06:53:29 +0800

    getContestParticipant() done

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
     private EditText editTextPassword;
     TextView textview; // 把視圖的元件宣告成全域變數
     String result; // 儲存資料用的字串
+    JSONArray result_arr;
     String User_name, User_pass, login_User_name, login_User_pass;
     Button btn_login, btn_register;
     private final static String TAG = "HTTPURLCONNECTION test";
@@ -50,7 +52,8 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
 //                thread.start(); // 開始執行
 //                add_Contest("upup", 1, "12:30:00", 30, "King", 5, "test.png");
 //                add_Attendance("sally", "再睡5分鐘", 0);
-//                add_Friend("sally", "marow");
+//                add_Friend("sally", "yicheng");
+//                getContestParticipant("再睡5分鐘");
             }
         });
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -218,19 +221,15 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
                 int responseCode = connection.getResponseCode();
                 if (responseCode == HttpURLConnection.HTTP_OK) {
                     // 如果 HTTP 回傳狀態是 OK ，而不是 Error
-                    InputStream inputStream =
-                            connection.getInputStream();
-                    // 取得輸入串流
-                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
-                    // 讀取輸入串流的資料
-                    String box = ""; // 宣告存放用字串
-                    String line = null; // 宣告讀取用的字串
-                    while((line = bufReader.readLine()) != null) {
-                        box += line + "\n";
-                        // 每當讀取出一列，就加到存放字串後面
-                    }
-                    inputStream.close(); // 關閉輸入串流
-                    result = box; // 把存放用字串放到全域變數
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = bufferedReader.readLine();
+
+                    // 将响应内容转换为 JSON 对象
+                    // 先将返回的字符串中的 <br> 替换为空字符串
+//                    String cleanedResponse = response.replace("<br>", "");
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    result = status;
                     Log.d("HTTP_output", "output!");
                     Log.d("SUCCESS result", "" +": "+result);
                 } else {
@@ -303,21 +302,15 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
                 int responseCode = connection.getResponseCode();
                 if (responseCode == HttpURLConnection.HTTP_OK) {
                     // 如果 HTTP 回傳狀態是 OK ，而不是 Error
-                    InputStream inputStream =
-                            connection.getInputStream();
-                    // 取得輸入串流
-                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
-                    // 讀取輸入串流的資料
-                    String box = ""; // 宣告存放用字串
-                    String line = null; // 宣告讀取用的字串
-                    while((line = bufReader.readLine()) != null) {
-                        box += line + "\n";
-                        // 每當讀取出一列，就加到存放字串後面
-                    }
-                    inputStream.close(); // 關閉輸入串流
-                    result = box; // 把存放用字串放到全域變數
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = bufferedReader.readLine();
+
+                    // 将响应内容转换为 JSON 对象
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    result = status;
                     Log.d("HTTP_output", "output!");
-                    Log.d("SUCCESS result", "" +": "+result);
+                    Log.d("SUCCESS result", "" +": "+result.charAt(0));
                 } else {
                     // 处理连接错误
                     // 例如，您可以获取错误消息
@@ -385,19 +378,13 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
                 int responseCode = connection.getResponseCode();
                 if (responseCode == HttpURLConnection.HTTP_OK) {
                     // 如果 HTTP 回傳狀態是 OK ，而不是 Error
-                    InputStream inputStream =
-                            connection.getInputStream();
-                    // 取得輸入串流
-                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
-                    // 讀取輸入串流的資料
-                    String box = ""; // 宣告存放用字串
-                    String line = null; // 宣告讀取用的字串
-                    while((line = bufReader.readLine()) != null) {
-                        box += line + "\n";
-                        // 每當讀取出一列，就加到存放字串後面
-                    }
-                    inputStream.close(); // 關閉輸入串流
-                    result = box; // 把存放用字串放到全域變數
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = bufferedReader.readLine();
+
+                    // 将响应内容转换为 JSON 对象
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    result = status;
                     Log.d("HTTP_output", "output!");
                     Log.d("SUCCESS result", "" +": "+result);
                 } else {
@@ -421,5 +408,96 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
         Thread thread = new Thread(addFriend);
         thread.start();
         return result;
+    };
+
+
+    public class GetContestParticipantRunnable implements Runnable {
+        private String contest_name;
+
+        public void setParams(String contest_name) {
+            this.contest_name = contest_name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://140.116.82.9:9000/getContestParticipant.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
+                connection.setRequestMethod("POST");
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                Map<String, Object> FriendData = new HashMap<>();
+                FriendData.put("contest_name", contest_name);
+
+                JSONObject response_j = new JSONObject(FriendData);
+                Log.v("提交數據", response_j.toString());
+                connection.connect(); // 開始連線
+                OutputStream os = connection.getOutputStream();
+                OutputStreamWriter objout = new OutputStreamWriter(os, "UTF-8");
+                objout.write(response_j.toString());
+                objout.flush();
+                os.close();
+                objout.close();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = bufferedReader.readLine();
+
+                    // 将响应内容转换为 JSON 对象
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    result = status;
+                    JSONArray dataArray = jsonObject.getJSONArray("data");
+                    result_arr = dataArray;
+                    //讀取output的方法
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        // 获取当前数组元素（一个 JSONObject）
+                        JSONObject dataObject = dataArray.getJSONObject(i);
+
+                        // 从 dataObject 中获取 participant_name 和 score 值
+                        String participantName = dataObject.getString("participant_name");
+                        int score = dataObject.getInt("score");
+
+                        // 输出 participant_name 和 score
+                        Log.v("Participant Name: " , ""+ participantName);
+                        Log.v("Score: ", ""+score);
+
+                        // 在此处可以根据需要将 participant_name 和 score 存入你的 HashMap
+                    }
+                    Log.d("HTTP_output", "output!");
+                    Log.d("SUCCESS result", "" +": "+result);
+                } else {
+                    // 处理连接错误
+                    // 例如，您可以获取错误消息
+                    InputStream errorStream = connection.getErrorStream();
+                    String errorMessage = readStream(errorStream); // 将错误流转换为字符串
+                    Log.d("BAD ", errorMessage);
+                    // 处理错误流
+                }
+
+            } catch(Exception e) {
+                result = e.toString(); // 如果出事，回傳錯誤訊息
+                Log.d("BAD result", "" +": "+result);
+            }
+        }
+    }
+    private JSONArray getContestParticipant(String contest_name){
+        GetContestParticipantRunnable GetContestParticipant = new GetContestParticipantRunnable();
+        GetContestParticipant.setParams(contest_name);
+        Thread thread = new Thread(GetContestParticipant);
+        thread.start();
+        return result_arr;
     };
 }
