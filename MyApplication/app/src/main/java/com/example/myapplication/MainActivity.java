4cb74c3e6d556d1fa5f6edfcdbea0a4bbe698a51
sally11131114 <sally11131114@gmail.com>
2023-06-06 22:47:55 +0800

    addContest() done

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,21 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -24,17 +19,8 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements ExampleDialog.ExampleDialogListener{
     private EditText editTextUsername;
@@ -44,8 +30,6 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
     String User_name, User_pass, login_User_name, login_User_pass;
     Button btn_login, btn_register;
     private final static String TAG = "HTTPURLCONNECTION test";
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,10 +44,11 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
             @Override
             // 按鈕事件
             public void onClick(View view) {
-                login_User_name = editTextUsername.getText().toString();
-                login_User_pass = editTextPassword.getText().toString();
-                Thread thread = new Thread(mutiThread);
-                thread.start(); // 開始執行
+//                login_User_name = editTextUsername.getText().toString();
+//                login_User_pass = editTextPassword.getText().toString();
+//                Thread thread = new Thread(mutiThread);
+//                thread.start(); // 開始執行
+                add_Contest("upup", 1, "12:30:00", 30, "King", 5, "test.png");
             }
         });
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -172,4 +157,100 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
         return response.toString();
     }
 
+    public class AddContestRunnable implements Runnable {
+        private String group_name;
+        private int type;
+        private String deadline;
+        private int expected_amount;
+        private String award_name;
+        private int award_score;
+        private String award_avatar;
+
+        public void setParams(String group_name, int type, String deadline, int expected_amount, String award_name, int award_score, String award_avatar) {
+            this.group_name = group_name;
+            this.type = type;
+            this.deadline = deadline;
+            this.expected_amount = expected_amount;
+            this.award_name = award_name;
+            this.award_score = award_score;
+            this.award_avatar = award_avatar;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://140.116.82.9:9000/addContest.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
+                connection.setRequestMethod("POST");
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                Map<String, Object> contestData = new HashMap<>();
+                contestData.put("group_name", group_name);
+                contestData.put("type", type);
+                contestData.put("deadline", deadline);
+                contestData.put("expected_amount", expected_amount);
+                contestData.put("award_name", award_name);
+                contestData.put("award_score", award_score);
+                contestData.put("award_avatar", award_avatar);
+
+                JSONObject response_j = new JSONObject(contestData);
+                Log.v("提交數據", response_j.toString());
+                connection.connect(); // 開始連線
+                OutputStream os = connection.getOutputStream();
+                OutputStreamWriter objout = new OutputStreamWriter(os, "UTF-8");
+                objout.write(response_j.toString());
+                objout.flush();
+                os.close();
+                objout.close();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    InputStream inputStream =
+                            connection.getInputStream();
+                    // 取得輸入串流
+                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
+                    // 讀取輸入串流的資料
+                    String box = ""; // 宣告存放用字串
+                    String line = null; // 宣告讀取用的字串
+                    while((line = bufReader.readLine()) != null) {
+                        box += line + "\n";
+                        // 每當讀取出一列，就加到存放字串後面
+                    }
+                    inputStream.close(); // 關閉輸入串流
+                    result = box; // 把存放用字串放到全域變數
+                    Log.d("HTTP_output", "output!");
+                    Log.d("SUCCESS result", "" +": "+result);
+                } else {
+                    // 处理连接错误
+                    // 例如，您可以获取错误消息
+                    InputStream errorStream = connection.getErrorStream();
+                    String errorMessage = readStream(errorStream); // 将错误流转换为字符串
+                    Log.d("BAD ", errorMessage);
+                    // 处理错误流
+                }
+
+            } catch(Exception e) {
+                result = e.toString(); // 如果出事，回傳錯誤訊息
+                Log.d("BAD result", "" +": "+result);
+            }
+        }
+    }
+    private String add_Contest(String group_name, int type, String deadline, int expected_amount, String award_name, int award_score, String award_avatar){
+        AddContestRunnable addContest = new AddContestRunnable();
+        addContest.setParams(group_name, type, deadline, expected_amount, award_name, award_score, award_avatar);
+        Thread thread = new Thread(addContest);
+        thread.start();
+        return result;
+    };
 }
