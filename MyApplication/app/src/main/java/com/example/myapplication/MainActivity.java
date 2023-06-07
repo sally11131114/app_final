66fa4af889d814e663cf25a8b541a9629ebcd7e0
sally11131114 <sally11131114@gmail.com>
2023-06-07 15:28:58 +0800

    All function done:>>

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -50,10 +50,13 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
 //                login_User_pass = editTextPassword.getText().toString();
 //                Thread thread = new Thread(mutiThread);
 //                thread.start(); // 開始執行
-//                add_Contest("upup", 1, "12:30:00", 30, "King", 5, "test.png");
-//                add_Attendance("sally", "再睡5分鐘", 0);
+//                add_Contest("我想睡覺好累==", 1, "12:30:00", 30, "King", 5, "test.png");
+//                add_Attendance("sally", "我想睡覺好累==", 0);
 //                add_Friend("sally", "yicheng");
 //                getContestParticipant("再睡5分鐘");
+//                getAttendedContest("sally");
+//                getFriend("marow");
+                getAward("marow");
             }
         });
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -306,11 +309,13 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
                     String response = bufferedReader.readLine();
 
                     // 将响应内容转换为 JSON 对象
+                    // 先将返回的字符串中的 <br> 替换为空字符串
+//                    String cleanedResponse = response.replace("<br>", "");
                     JSONObject jsonObject = new JSONObject(response);
                     String status = jsonObject.getString("status");
                     result = status;
                     Log.d("HTTP_output", "output!");
-                    Log.d("SUCCESS result", "" +": "+result.charAt(0));
+                    Log.d("SUCCESS result", "" +": "+result);
                 } else {
                     // 处理连接错误
                     // 例如，您可以获取错误消息
@@ -497,6 +502,266 @@ public class MainActivity extends AppCompatActivity implements ExampleDialog.Exa
         GetContestParticipantRunnable GetContestParticipant = new GetContestParticipantRunnable();
         GetContestParticipant.setParams(contest_name);
         Thread thread = new Thread(GetContestParticipant);
+        thread.start();
+        return result_arr;
+    };
+
+    public class GetAttendedContestRunnable implements Runnable {
+        private String participant_name;
+
+        public void setParams(String participant_name) {
+            this.participant_name = participant_name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://140.116.82.9:9000/getAttendedContest.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
+                connection.setRequestMethod("POST");
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                Map<String, Object> FriendData = new HashMap<>();
+                FriendData.put("participant_name", participant_name);
+
+                JSONObject response_j = new JSONObject(FriendData);
+                Log.v("提交數據", response_j.toString());
+                connection.connect(); // 開始連線
+                OutputStream os = connection.getOutputStream();
+                OutputStreamWriter objout = new OutputStreamWriter(os, "UTF-8");
+                objout.write(response_j.toString());
+                objout.flush();
+                os.close();
+                objout.close();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = bufferedReader.readLine();
+
+                    // 将响应内容转换为 JSON 对象
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    result = status;
+                    JSONArray dataArray = jsonObject.getJSONArray("data");
+                    result_arr = dataArray;
+                    //讀取output的方法
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        // 获取当前数组元素（一个 JSONObject）
+                        JSONObject dataObject = dataArray.getJSONObject(i);
+
+                        // 从 dataObject 中获取 participant_name 和 score 值
+                        String ContestName = dataObject.getString("contest_name");
+
+                        // 输出 participant_name 和 score
+                        Log.v("ContestName: " , ""+ ContestName);
+
+                        // 在此处可以根据需要将 participant_name 和 score 存入你的 HashMap
+                    }
+                    Log.d("HTTP_output", "output!");
+                    Log.d("SUCCESS result", "" +": "+result);
+                } else {
+                    // 处理连接错误
+                    // 例如，您可以获取错误消息
+                    InputStream errorStream = connection.getErrorStream();
+                    String errorMessage = readStream(errorStream); // 将错误流转换为字符串
+                    Log.d("BAD ", errorMessage);
+                    // 处理错误流
+                }
+
+            } catch(Exception e) {
+                result = e.toString(); // 如果出事，回傳錯誤訊息
+                Log.d("BAD result", "" +": "+result);
+            }
+        }
+    }
+    private JSONArray getAttendedContest(String participant_name){
+        GetAttendedContestRunnable GetAttendedContest = new GetAttendedContestRunnable();
+        GetAttendedContest.setParams(participant_name);
+        Thread thread = new Thread(GetAttendedContest);
+        thread.start();
+        return result_arr;
+    };
+    public class GetFriendRunnable implements Runnable {
+        private String my_name;
+
+        public void setParams(String my_name) {
+            this.my_name = my_name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://140.116.82.9:9000/getFriend.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
+                connection.setRequestMethod("POST");
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                Map<String, Object> FriendData = new HashMap<>();
+                FriendData.put("my_name", my_name);
+
+                JSONObject response_j = new JSONObject(FriendData);
+                Log.v("提交數據", response_j.toString());
+                connection.connect(); // 開始連線
+                OutputStream os = connection.getOutputStream();
+                OutputStreamWriter objout = new OutputStreamWriter(os, "UTF-8");
+                objout.write(response_j.toString());
+                objout.flush();
+                os.close();
+                objout.close();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = bufferedReader.readLine();
+
+                    // 将响应内容转换为 JSON 对象
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    result = status;
+                    JSONArray dataArray = jsonObject.getJSONArray("data");
+                    result_arr = dataArray;
+                    //讀取output的方法
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        // 获取当前数组元素（一个 JSONObject）
+                        JSONObject dataObject = dataArray.getJSONObject(i);
+
+                        // 从 dataObject 中获取 my_name 和 score 值
+                        String FriendName = dataObject.getString("friend_name");
+
+                        // 输出 my_name
+                        Log.v("FriendName: " , ""+ FriendName);
+
+                    }
+                    Log.d("HTTP_output", "output!");
+                    Log.d("SUCCESS result", "" +": "+result);
+                } else {
+                    // 处理连接错误
+                    // 例如，您可以获取错误消息
+                    InputStream errorStream = connection.getErrorStream();
+                    String errorMessage = readStream(errorStream); // 将错误流转换为字符串
+                    Log.d("BAD ", errorMessage);
+                    // 处理错误流
+                }
+
+            } catch(Exception e) {
+                result = e.toString(); // 如果出事，回傳錯誤訊息
+                Log.d("BAD result", "" +": "+result);
+            }
+        }
+    }
+    private JSONArray getFriend(String my_name){
+        GetFriendRunnable GetFriend = new GetFriendRunnable();
+        GetFriend.setParams(my_name);
+        Thread thread = new Thread(GetFriend);
+        thread.start();
+        return result_arr;
+    };
+    public class GetAwardRunnable implements Runnable {
+        private String user_name;
+
+        public void setParams(String user_name) {
+            this.user_name = user_name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://140.116.82.9:9000/getAward.php");
+                // 開始宣告 HTTP 連線需要的物件，這邊通常都是一綑的
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // 建立 Google 比較挺的 HttpURLConnection 物件
+                connection.setRequestMethod("POST");
+                // 設定連線方式為 POST
+                connection.setDoOutput(true); // 允許輸出
+                connection.setDoInput(true); // 允許讀入
+                connection.setUseCaches(false); // 不使用快取
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                Map<String, Object> FriendData = new HashMap<>();
+                FriendData.put("user_name", user_name);
+
+                JSONObject response_j = new JSONObject(FriendData);
+                Log.v("提交數據", response_j.toString());
+                connection.connect(); // 開始連線
+                OutputStream os = connection.getOutputStream();
+                OutputStreamWriter objout = new OutputStreamWriter(os, "UTF-8");
+                objout.write(response_j.toString());
+                objout.flush();
+                os.close();
+                objout.close();
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // 如果 HTTP 回傳狀態是 OK ，而不是 Error
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = bufferedReader.readLine();
+
+                    // 将响应内容转换为 JSON 对象
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+                    result = status;
+                    JSONArray dataArray = jsonObject.getJSONArray("data");
+                    result_arr = dataArray;
+                    //讀取output的方法
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        // 获取当前数组元素（一个 JSONObject）
+                        JSONObject dataObject = dataArray.getJSONObject(i);
+
+                        // 从 dataObject 中获取 user_name 和 score 值
+                        String AwardAvatar = dataObject.getString("award_avatar");
+
+                        // 输出 user_name
+                        Log.v("AwardAvatar: " , ""+ AwardAvatar);
+
+                    }
+                    Log.d("HTTP_output", "output!");
+                    Log.d("SUCCESS result", "" +": "+result);
+                } else {
+                    // 处理连接错误
+                    // 例如，您可以获取错误消息
+                    InputStream errorStream = connection.getErrorStream();
+                    String errorMessage = readStream(errorStream); // 将错误流转换为字符串
+                    Log.d("BAD ", errorMessage);
+                    // 处理错误流
+                }
+
+            } catch(Exception e) {
+                result = e.toString(); // 如果出事，回傳錯誤訊息
+                Log.d("BAD result", "" +": "+result);
+            }
+        }
+    }
+    private JSONArray getAward(String user_name){
+        GetAwardRunnable GetAward = new GetAwardRunnable();
+        GetAward.setParams(user_name);
+        Thread thread = new Thread(GetAward);
         thread.start();
         return result_arr;
     };
